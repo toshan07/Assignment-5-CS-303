80dbd4a99303057efbad74b62465037b7033d4cc
toshan07 <toshangupta.04@gmail.com>
2026-02-28 13:04:24 +0530

    Feature-1: Added delete_task functionality

--- todo.py
+++ todo.py
@@ -11,6 +11,17 @@ def list_tasks():
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
 
+#added new feature
+def delete_task(task_number):
+    if 0 < task_number <= len(tasks):
+        removed = tasks.pop(task_number - 1)
+        print(f'Task "{removed}" deleted.')
+    else:
+        print("Invalid task number.")
+
 if __name__ == "__main__":
     add_task("Finish Assignment")
+    add_task("Study Git")
+    list_tasks()
+    delete_task(1)
     list_tasks()
